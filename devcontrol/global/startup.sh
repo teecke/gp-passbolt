23c28dd1388faf6b979e0ae4c073de6e92a007bf
Pedro Amador <pedroamador.rodriguez@gmail.com>
2020-01-26 13:06:23 +0100

    Fix: Don't use --relative in backup script

--- devcontrol/global/startup.sh
+++ devcontrol/global/startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+set -eu
+
 # @file devcontrol/global/startup.sh
 # @brief devcontrol startup script and functions
-echo "Generic Platform - Passbolt Service (c) Teecke 2019"
+echo "Generic Platform - Passbolt Service (c) Teecke 2019 - 2020"
 echo
